5cb36acc81902bcdd1a81adddc57d946875f3d71
Gracyele <gracyelesantossj@gmail.com>
2021-08-02 15:24:54 -0300

    cria saldo controller

---
+++ breakeven/lib/controller/saldo_controller.dart
@@ -0,0 +1,31 @@
+import 'package:breakeven/models/saldo_movimentacao.dart';
+import 'package:get/get.dart';
+
+class SaldoController extends GetxController {
+  var valorEntrada = "0".obs;
+  var valorAtual = "0".obs;
+  var valorCalculado = 0.0.obs;
+  var movimentacao = <SaldoMovimentacao>[].obs;
+
+  calcularMovimentacao() {
+    valorCalculado.value = 0.0;
+    for (SaldoMovimentacao s in movimentacao) {
+      if (s.isReceita) {
+        valorCalculado = valorCalculado + double.parse(s.valor);
+      } else {
+        valorCalculado = valorCalculado - double.parse(s.valor);
+      }
+      print("VALOR CALC: $valorCalculado, ${s.isReceita}");
+    }
+
+    valorAtual.value =
+        (double.parse(valorEntrada.value) + valorCalculado.value).toString();
+  }
+
+  retirar(var valorRetirado) => valorAtual =
+      (double.parse(valorAtual as String) - double.parse(valorRetirado))
+          as RxString;
+  adicionar(var valorAdicionado) => valorAtual =
+      (double.parse(valorAtual as String) - double.parse(valorAdicionado))
+          as RxString;
+}
