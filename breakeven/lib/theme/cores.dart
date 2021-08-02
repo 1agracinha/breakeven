b65f6635a0ede88870413e957fa3cb4fbb2e2b4b
Gracyele <gracyelesantossj@gmail.com>
2021-08-02 15:25:29 -0300

    cria tema de cores

---
+++ breakeven/lib/theme/cores.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff00B3CB);
+
+const Color verde = const Color(0xff00E995);
+
+const Color vermelho = const Color(0xffF68870);
